7dcf0dbc9cd6d0fa4b8855a1cb7e5b8c682c598b
bertyjobbo <robjohnson1978@gmail.com>
2014-07-17 17:39:16 +0100

    .

---
+++ SingularControls.Web.Ui/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SingularControls.Web.Ui")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SingularControls.Web.Ui")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2be8400b-3a9a-4073-a16a-0d9e461dc35e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SingularControls.Web.Ui/SgControls/sgcontrols.js
@@ -0,0 +1,21 @@
+﻿// app
+var SingularControls = angular.module("sgControls",[]);
+
+// form directive
+(function(app) {
+
+    // create directive
+    app.directive("sgForm", [function() {
+
+        return {
+            
+            restrict: "E",
+            link:function(scope, element, attrs) {
+                alert("Got sgForm");
+            }
+
+        };
+
+    }]);
+
+})(SingularControls);
