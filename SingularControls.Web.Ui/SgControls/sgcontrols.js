bc370f2fccfb60e353faa4ba79f7c33137e41457
bertyjobbo <robjohnson1978@gmail.com>
2014-07-17 18:29:55 +0100

    .

--- SingularControls.Web.Ui/SgControls/sgcontrols.js
+++ SingularControls.Web.Ui/SgControls/sgcontrols.js
@@ -1,21 +1,55 @@
 ﻿// app
-var SingularControls = angular.module("sgControls",[]);
+var SingularControls = {};
+SingularControls.Module = angular.module("sgControls", ['ng']);
 
 // form directive
-(function(app) {
+(function(namespace, app) {
+
+    // controls provider
+    namespace.SgControlsProvider =  function () {
+
+        // this
+        var ts = this;
+
+        // wrappers
+        ts.wrappers = [];
+
+        // this
+        ts.addControlWrapper = function (which, callback) {
+            ts.wrappers[which] = callback;
+        };
+
+        // provide
+        ts.$get = [function() {
+            return ts;
+        }];
+    };
+
+    // add provider to app
+    app.provider("sgControlsConfig", namespace.SgControlsProvider);
 
     // create directive
-    app.directive("sgForm", [function() {
+    app.directive("sgForm", ["sgControlsConfig", function (sgControlsConfig) {
 
         return {
-            
+
+            //scope: {
+                
+            //},
+
             restrict: "E",
+
             link:function(scope, element, attrs) {
-                alert("Got sgForm");
+
+                // firstly, get data
+                var data = scope.$eval(attrs.datasource);
+
+                //
+                console.log("WHATS NEXT", data, sgControlsConfig.wrappers["sgAll"]);
             }
 
         };
 
     }]);
 
-})(SingularControls);
+})(SingularControls, SingularControls.Module);
